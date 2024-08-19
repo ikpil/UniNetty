c344461ffc02745f4c1dacdb44935c6e2106ef03
ikpil <ikpil@naver.com>
2024-08-20 07:30:13 +0900

    intergrate everything via demo

--- examples/UniNetty.Examples.Demo.Client/Program.cs
+++ examples/UniNetty.Examples.Demo.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using UniNetty.Examples.Common;
+using UniNetty.Examples.Telnet.Client;
 using UniNetty.Examples.WebSockets.Client;
 
 namespace UniNetty.Examples.Demo.Client;
@@ -9,9 +10,19 @@ class Program
     {
         ExampleHelper.SetConsoleLogger();
 
+        RunWebSocketClient();
+        RunTelentClient();
+    }
+
+    static void RunWebSocketClient()
+    {
         var client = new WebSocketClient();
         client.RunClientAsync(ClientSettings.Cert, ClientSettings.Host, ClientSettings.Port, ExampleHelper.Configuration["path"]).Wait();
     }
     
-
+    static void RunTelentClient()
+    {
+        var client = new TelnetClient();
+        client.RunClientAsync(ClientSettings.Cert, ClientSettings.Host, ClientSettings.Port).Wait();
+    }
 }
