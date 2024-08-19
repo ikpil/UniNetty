0c29ce0663a234c629851fa07eb0b073394a3618
ikpil <ikpil@naver.com>
2024-08-20 00:47:00 +0900

    for demo client/server

---
+++ examples/UniNetty.Examples.Demo.Client/Program.cs
@@ -0,0 +1,15 @@
+﻿using UniNetty.Examples.Common;
+using UniNetty.Examples.WebSockets.Client;
+
+namespace UniNetty.Examples.Demo.Client;
+
+class Program
+{
+    static void Main()
+    {
+        ExampleHelper.SetConsoleLogger();
+
+        var client = new WebSocketClient();
+        client.RunClientAsync(ClientSettings.Cert, ClientSettings.Host, ClientSettings.Port, ExampleHelper.Configuration["path"]).Wait();
+    }
+}
---
+++ examples/UniNetty.Examples.Demo.Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using UniNetty.Common;
+using UniNetty.Examples.Common;
+using UniNetty.Examples.WebSockets.Server;
+
+namespace UniNetty.Examples.Demo.Server;
+
+class Program
+{
+    static void Main()
+    {
+        ExampleHelper.SetConsoleLogger();
+
+        ResourceLeakDetector.Level = ResourceLeakDetector.DetectionLevel.Disabled;
+
+        Console.WriteLine(
+            $"\n{RuntimeInformation.OSArchitecture} {RuntimeInformation.OSDescription}"
+            + $"\n{RuntimeInformation.ProcessArchitecture} {RuntimeInformation.FrameworkDescription}"
+            + $"\nProcessor Count : {Environment.ProcessorCount}\n");
+
+        Console.WriteLine("Transport type : Socket");
+
+        if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+            GCSettings.LatencyMode = GCLatencyMode.SustainedLowLatency;
+        }
+
+        Console.WriteLine($"Server garbage collection : {(GCSettings.IsServerGC ? "Enabled" : "Disabled")}");
+        Console.WriteLine($"Current latency mode for garbage collection: {GCSettings.LatencyMode}");
+        Console.WriteLine("\n");
+
+        var server = new WebSocketServer();
+        server.RunServerAsync(ServerSettings.Cert, ServerSettings.Port).Wait();
+    }
+}
