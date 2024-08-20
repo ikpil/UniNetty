5300b5654a689908926afcde29c1e0f2e49a5fb5
ikpil <ikpil@naver.com>
2024-08-20 14:08:07 +0900

    add license

--- examples/UniNetty.Examples.Demo.Client/Program.cs
+++ examples/UniNetty.Examples.Demo.Client/Program.cs
@@ -1,4 +1,8 @@
-﻿using UniNetty.Examples.Common;
+// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Ikpil Choi ikpil@naver.com All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using UniNetty.Examples.Common;
 using UniNetty.Examples.Discard.Client;
 using UniNetty.Examples.Echo.Client;
 using UniNetty.Examples.Factorial.Client;
--- examples/UniNetty.Examples.QuoteOfTheMoment.Server/QuoteOfTheMomentServer.cs
+++ examples/UniNetty.Examples.QuoteOfTheMoment.Server/QuoteOfTheMomentServer.cs
@@ -1,4 +1,8 @@
-﻿using System;
+// Copyright (c) Microsoft. All rights reserved.
+// Copyright (c) Ikpil Choi ikpil@naver.com All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
 using System.Threading.Tasks;
 using UniNetty.Handlers.Logging;
 using UniNetty.Transport.Bootstrapping;
