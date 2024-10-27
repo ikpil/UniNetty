473d531d8feb18da4f016be2a05562e866858aee
ikpil <ikpil0@gmail.com>
2024-10-27 14:14:13 +0900

    clean up - Remove examples to prepare for Unity package creation

--- examples/UniNetty.Examples.Demo/UI/IView.cs
+++
@@ -1,7 +0,0 @@
-namespace UniNetty.Examples.Demo.UI;
-
-public interface IView
-{
-    void Draw(double dt);
-    void Update(double dt);
-}
--- examples/UniNetty.Examples.Demo/UI/LogViewItem.cs
+++
@@ -1,7 +0,0 @@
-namespace UniNetty.Examples.Demo.UI;
-
-public record LogViewItem
-{
-    public int Level;
-    public string Message;
-}
