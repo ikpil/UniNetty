757513494420757f15827749280c93156c0daf33
ikpil <ikpil0@gmail.com>
2024-10-27 15:08:11 +0900

    added UniNettyMenu for unity3d

---
+++ Editor/UniNetty.Editor/UniNettyMenu.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UniNetty
+{
+    public class UniNettyMenu
+    {
+        [MenuItem("UniNetty/Examples")]
+        public static void MyMenuItem()
+        {
+            Debug.Log("examples");
+        }
+    }
+}
